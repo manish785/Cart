8fc4fe262ae2721dabea74334653980d47d3f09d
manish785 <kumarachilish1997@gmail.com>
2023-08-07 17:08:43 +0530

    refactored the code

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+  // through the Constructor, we define the state of the component
   constructor () {
     super();
     this.state = {
@@ -9,9 +10,62 @@ class CartItem extends React.Component {
       qty: 1,
       img: ''
     }
+    // this.testing();
   }
   
+  //  testing(){
+  //    const promise = new Promise((resolve, reject) => {
+  //       setTimeout(()=>{
+  //         resolve('done');
+  //       }, 5000)
+  //    })
+
+  //    promise.then(()=>{
+       
+  //      // this setState acts like a Synchronous call
+  //      this.setState({qty: this.state.qty+10});
+
+  //      this.setState({qty: this.state.qty+10});
+
+  //      this.setState({qty: this.state.qty+10});
+
+  //      console.log('state', this.state)
+  //    })
+  //   }
+     
+
+
   increaseQuantity = () =>{
+    // this.state.qty += 1;
+    // //console.log('this', this);
+    // // setState form 1
+    // this.setState({
+    //   // here, we are incrementing the current quantity by plus one
+    //   qty: this.state.qty + 1
+    // }, ()=>{});
+
+
+
+    // setState form 2 - if prevState required use this 
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+      // when setState call finished, the below callback is getting called
+    }, ()=>{
+      console.log('state', this.state);
+    });
+
+   
+  }
+
+   
+  decreaseQuantity = () =>{
+     const {qty} = this.state;
+
+     if(qty === 0){
+       return;
+     }
     // this.state.qty += 1;
     // console.log('this', this);
     // setState form 1
@@ -22,7 +76,7 @@ class CartItem extends React.Component {
     // setState form 2 - if prevState required use this 
     this.setState((prevState) => {
       return {
-        qty: prevState.qty + 1
+        qty: prevState.qty - 1
       }
     });
   }
@@ -33,9 +87,11 @@ class CartItem extends React.Component {
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} />
+        {/* // this is the way we do styling inside the JSX */}
+          <img style={styles.image} />  
         </div>
         <div className="right-block">
+           {/* // this is the way we do styling inside the JSX */}
           <div style={ { fontSize: 25 } }>{title}</div>
           <div style={ { color: '#777' } }>Rs {price} </div>
           <div style={ { color: '#777' } }>Qty: {qty} </div>
@@ -51,6 +107,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons" 
               src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" 
+              onClick={this.decreaseQuantity}
             />
             <img 
               alt="delete" 
